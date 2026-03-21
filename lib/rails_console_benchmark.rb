6066a1bbb9ba25540d193ee653372ab27a5e4e1a
Matthew Jacques <iFtw@live.co.uk>
2026-03-21 17:04:32 +0000

    Remove conditional Railtie loading from main file and initial plan

--- lib/rails_console_benchmark.rb
+++ lib/rails_console_benchmark.rb
@@ -3,7 +3,6 @@
 require_relative "rails_console_benchmark/version"
 require_relative "rails_console_benchmark/formatter"
 require_relative "rails_console_benchmark/tracker"
-require_relative "rails_console_benchmark/railtie" if defined?(Rails::Railtie)
 
 module RailsConsoleBenchmark
 end
