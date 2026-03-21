a4d1200639d1e854312decdcc28e11ea3da6f22d
Matthew Jacques <matty.jacques@proton.me>
2026-03-21 09:29:31 +0000

    Initial commit

---
+++ lib/rails_console_benchmark.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rails_console_benchmark/version"
+
+module RailsConsoleBenchmark
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rails_console_benchmark/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RailsConsoleBenchmark
+  VERSION = "0.1.0"
+end
